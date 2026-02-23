b0ef43a113477faf960a4a23dcc640f373e57991
Enrico Battistella <enrico.battistella@digitalrockers.it>
2026-02-23 17:37:29 +0100

    manual populate

---
+++ src/api/cart-item/cart-item.model.ts
@@ -0,0 +1,29 @@
+import { HydratedDocument, model, Schema } from "mongoose";
+import { CartItem } from "./cart-item.entity";
+
+const cartItemSchema = new Schema<CartItem>({
+  quantity: Number,
+  product: Schema.Types.ObjectId
+});
+
+cartItemSchema.set('toJSON', {
+  virtuals: true,
+  transform: (_, ret: any) => {
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  }
+});
+
+cartItemSchema.set('toObject', {
+  virtuals: true,
+  transform: (_, ret: any) => {
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  }
+});
+
+export type CartItemDocument = HydratedDocument<CartItem>;
+
+export const CartItemModel = model<CartItem>('CartItem', cartItemSchema);
--- src/api/cart-item/cart-item.service.ts
+++ src/api/cart-item/cart-item.service.ts
@@ -1,20 +1,22 @@
 import { CartItem } from './cart-item.entity';
 import productSrv from '../product/product.service';
+import { CartItemDocument, CartItemModel } from './cart-item.model';
+import { isValidObjectId } from 'mongoose';
 
-const cart: CartItem[] = [];
 export class CartItemService {
   async find(): Promise<CartItem[]> {
+    const cart = await CartItemModel.find();
     return this.populateCartItem(cart);
   }
 
-  private async _getById(id: string): Promise<CartItem | null> {
-    const item = cart.find(element => element.id === id);
+  private async _getById(id: string): Promise<CartItemDocument | null> {
+    const item = await CartItemModel.findById(id);
     return item ?? null;
   }
-  
+
   async getById(id: string): Promise<CartItem | null> {
     const item = await this._getById(id);
-    
+
     return !!item ? this.populateCartItem(item) : null;
   }
 
@@ -27,64 +29,57 @@ export class CartItemService {
       return res!;
     }
   }
-  
+
   async add(item: Omit<CartItem, 'id'>): Promise<CartItem> {
-    const id: string = cart.length.toString();
-    const newItem = {
-      id,
-      ...item
-    };
-    cart.push(newItem);
+    const newItem = await CartItemModel.create(item);
     return this.populateCartItem(newItem);
   }
-  
+
   async update(id: string, data: Partial<Omit<CartItem, 'id'>>): Promise<CartItem | null> {
     const item = await this._getById(id);
     if (!item) {
       return null;
     }
-    
     Object.assign(item!, data);
-    
+    const updated = await item.save();
 
-    return this.populateCartItem(item!);
+    return this.populateCartItem(updated!);
   }
-  
+
   async remove(id: string): Promise<CartItem | null> {
     const item = await this.getById(id);
     if (!item) {
       return null;
     }
-    const index = cart.indexOf(item);
-    
-    cart.splice(index, 1);
-    
+    await CartItemModel.deleteOne({_id: id});
+
     return item;
   }
 
   async getByProduct(productId: string): Promise<CartItem | null> {
-    const exists = cart.find(item => item.product === productId);
+    const exists = await CartItemModel.findOne({ product: productId });
     return !!exists? this.populateCartItem(exists) : null;
   }
 
-  async populateCartItem(item: CartItem): Promise<CartItem>;
-  async populateCartItem(item: CartItem[]): Promise<CartItem[]>;
-  async populateCartItem(item: CartItem | CartItem[]): Promise<CartItem | CartItem[]> {
+  async populateCartItem(item: CartItemDocument): Promise<CartItem>;
+  async populateCartItem(item: CartItemDocument[]): Promise<CartItem[]>;
+  async populateCartItem(item: CartItemDocument | CartItemDocument[]): Promise<CartItem | CartItem[]> {
     if (Array.isArray(item)) {
       const promises = item.map(i => this.populateCartItem(i));
       return Promise.all(promises);
     }
 
-    if (typeof item.product === 'object') {
+    if (!isValidObjectId(item.product)) {
       return item;
     }
     const id = item.product;
-    const product = await productSrv.getById(id);
+    const product = await productSrv.getById(id as string);
 
-    return {
-      ...item,
+    const res ={
+      ...item.toObject(),
       product: product!
-    }
+    };
+    return res;
   }
 }
 
